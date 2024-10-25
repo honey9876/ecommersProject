6c85d6b930a0ad07ff300009ca89422f2e2c5788
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-10-25 18:33:46 +0530

    update project

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model")
+
+
+
+
+
+if(precess.env === "development"){
+    router.post("./create", async function (req, res){
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res.
+            status(500)
+            .send("you don't have permission to create a new owner.");
+        }
+
+      let { fullname, email, password} = req.body;  
+
+      let createdOwner =  await ownerModel.create({
+            fullname,
+            email,
+            password,
+
+        });
+        res.status(201).send(createdOwner);
+    });
+}
+
 
 router.get("/", function (req, res) {
     res.send("hey its working")
 });
+
+
 
 
 module.exports = router;
